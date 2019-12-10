1104f87ff513fee81db57d5de19b024ed1b8ae00
liu-dong <964708273@qq.com>
2019-12-10 17:44:08 +0800

    工具模块封装

--- mango-ui/src/main.js
+++ mango-ui/src/main.js
@@ -1,14 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import api from './http'
+import global from '@/utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+Vue.use(ElementUI)  // 引入Element
+Vue.use(api)  // 引入API模块
 
-Vue.use(ElementUI)
+Vue.prototype.global = global // 挂载全局配置模块
 
 new Vue({
   el: '#app',
