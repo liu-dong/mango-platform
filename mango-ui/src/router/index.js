52b98ed91189fc03eb23e7a708a73275c32ecef8
liu-dong <964708273@qq.com>
2019-12-10 17:43:34 +0800

    前端项目案例

--- mango-ui/src/router/index.js
+++ mango-ui/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/Login'
+import Home from '@/views/Home'
+import NotFound from '@/views/404'
 
 Vue.use(Router)
 
@@ -8,8 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/404',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
