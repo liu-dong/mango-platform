ef1d74dd0df0d21ef040ec5c42d7e546c3c04467
liu-dong <964708273@qq.com>
2019-12-10 17:45:40 +0800

    登录流程完善

--- mango-ui/src/views/Home.vue
+++ mango-ui/src/views/Home.vue
@@ -1,37 +1,34 @@
 <template>
-  <div class="page">
-    <h2>Home Page</h2>
-    <li class="fa fa-home fa-lg"></li>
-    <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
-    <el-button type="primary" @click="getUser()">获取用户信息</el-button>
-    <el-button type="primary" @click="getMenu()">获取菜单信息</el-button>
-    <h3>{{$t('common.doc')}}</h3>
-    <el-button type="success" @click="changeLanguage('zh_cn')">简体中文</el-button>
-    <el-button type="success" @click="changeLanguage('en_us')">English</el-button>
+  <div class="container">
+      <!-- 导航菜单栏 -->
+      <nav-bar></nav-bar>
+      <!-- 头部区域 -->
+      <head-bar></head-bar>
+      <!-- 主内容区域 -->
+      <main-content></main-content>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import mock from '@/mock/index.js'
+import HeadBar from "./HeadBar"
+import NavBar from "./NavBar"
+import MainContent from "./MainContent"
 export default {
-  name: 'Home',
-  methods: {
-    // 语言切换
-    changeLanguage(lang) {
-      lang === '' ? 'zh_cn' : lang
-      this.$i18n.locale = lang
-      this.langVisible = false
-    },
-    testAxios() {
-      axios.get('http://localhost:8080').then(res => { alert(res.data) })
-    },
-    getUser() {
-      axios.get('http://localhost:8080/user').then(res => { alert(JSON.stringify(res.data)) })
-    },
-    getMenu() {
-      axios.get('http://localhost:8080/menu').then(res => { alert(JSON.stringify(res.data)) })
-    }
+  components:{
+        HeadBar,
+        NavBar,
+        MainContent
   }
-}
+};
 </script>
+
+<style scoped lang="scss">
+  .container {
+    position:absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    // background: rgba(224, 234, 235, 0.1);
+  }
+</style>  
