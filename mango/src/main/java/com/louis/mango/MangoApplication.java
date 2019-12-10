ead6174cd3f2f6874ba9cbc9753f2be0e3636cf8
liu-dong <964708273@qq.com>
2019-12-10 09:57:46 +0800

    集成MyBatis框架

--- mango/src/main/java/com/louis/mango/MangoApplication.java
+++ mango/src/main/java/com/louis/mango/MangoApplication.java
@@ -3,7 +3,7 @@ package com.louis.mango;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.louis.mango"})
 public class MangoApplication {
 
 	public static void main(String[] args) {
