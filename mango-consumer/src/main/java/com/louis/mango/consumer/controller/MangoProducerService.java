50a0e7d1f14a17db223b6912ad254a32e63879c9
liu-dong <964708273@qq.com>
2019-12-10 11:59:15 +0800

    服务熔断（Hystrix、Turbine）

--- mango-consumer/src/main/java/com/louis/mango/consumer/controller/MangoProducerService.java
+++ mango-consumer/src/main/java/com/louis/mango/consumer/controller/MangoProducerService.java
@@ -3,7 +3,7 @@ package com.louis.mango.consumer.controller;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "mango-producer")
+@FeignClient(name = "mango-producer", fallback = MangoProducerHystrix.class)
 public interface MangoProducerService {
 
     @RequestMapping("/hello")
